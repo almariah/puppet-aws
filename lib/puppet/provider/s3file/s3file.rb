8b8968efb35955342b6e6f70ab0cdb41b13c95a5
Abdullah Almariah <abdullahalmariah@gmail.com>
2018-02-05 10:55:08 +0100

    Release

--- lib/puppet/provider/s3file/s3file.rb
+++ lib/puppet/provider/s3file/s3file.rb
@@ -6,11 +6,16 @@ Puppet::Type.type(:s3file).provide(:s3file) do
 
   confine :feature => :awssdks3
 
-  def latest?
-    notice "check  aws"
-    s3 = Aws::S3::Resource.new(region: resource[:region])
-    $s3_obj = s3.bucket(resource[:bucket]).object(resource[:object])
+  def exists?
+    if File.file?(resource[:path])
+      return true
+    else
+      false
+    end
+  end
 
+  def latest?
+    $s3_obj = getObject
     if exists?
       md5_new =  Digest::MD5.hexdigest $s3_obj.get.body.string
       md5_current = Digest::MD5.file(resource[:path]).hexdigest
@@ -22,34 +27,24 @@ Puppet::Type.type(:s3file).provide(:s3file) do
     end
   end
 
-
-  def exists?
-    if File.file?(resource[:path])
-      return true
-    else
-      false
+  def create
+    if !exists?
+      $s3_obj = getObject
     end
+    $s3_obj.get(response_target: resource[:path])
   end
 
   def update
     self.create
   end
 
-  def getObject
-
-  end
-
-
-  def create
-    if !exists?
-      notice "check  aws"
-      s3 = Aws::S3::Resource.new(region: resource[:region])
-      $s3_obj = s3.bucket(resource[:bucket]).object(resource[:object])
-    end
-    $s3_obj.get(response_target: resource[:path])
-  end
-
   def destroy
     FileUtils.rm_rf resource[:path]
+  end
+
+  def getObject
+    notice "connecting to AWS"
+    s3 = Aws::S3::Resource.new(region: resource[:region])
+    return s3.bucket(resource[:bucket]).object(resource[:object])
   end
 end
